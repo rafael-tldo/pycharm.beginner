f686a129644b9c5199b8143eb0b0d43f6c706577
Rafael <proferafaeltoledo@gmail.com>
2020-07-21 18:47:10 -0300

    ADICIONANDO TESTE 1.0

---
+++ Teste1.0.py
@@ -0,0 +1,28 @@
+""""
+Script salvo.
+
+"""
+
+nome = input ('Qual é o seu nome? ')
+print ('Olá, ' + nome +' , prazer em conhecê-lo!')
+idade = input ('Qual é a sua idade? ')
+peso = input ('Qual é o seu peso? ')
+print ('Obrigado, ' + nome + '!' ' Agora preciso da sua data de nascimento.')
+
+
+dia = input (' Dia ')
+mês = input (' Mês ')
+ano = input (' Ano ')
+print ('Você nasceu no dia ' + dia + ' de ' + mês + ' de ' + ano + '. Correto?' )
+
+print ('Ok. Vamos verificar seu perfil com uma soma. Digite os números: ')
+
+a = int (input ('Digite o primeiro número: '))
+b = int (input ('Digite o segundo número: '))
+c = a + b
+
+print ( 'A soma é:', a + b , '.OK?')
+
+input()
+print('OK')
+
